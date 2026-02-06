853efe6ac2282cd66a058e161404ff9ce5175d76
Sampath Gunnam <gsampathchowdary@gmail.com>
2026-02-07 01:29:36 +0530

    Updated game score file

--- game_score.py
+++ game_score.py
@@ -1,5 +1,5 @@
-player_name = str(input("Player :"))
-games_played = int(input("Games Played :"))
-total_score = int(input("Total Score :"))
+player_name = str(input("Player : "))
+games_played = int(input("Games Played : "))
+total_score = int(input("Total Score : "))
 
 print(f"Average Score :{total_score/games_played}")
